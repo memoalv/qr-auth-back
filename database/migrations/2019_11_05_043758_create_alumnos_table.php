64323a397bcd865d4fbb1e8cde8f2ab03e6f1029
Guillermo Alvarez <gjalvarez09@gmail.com>
2019-11-05 12:33:22 -0700

    insert on migration

--- database/migrations/2019_11_05_043758_create_alumnos_table.php
+++ database/migrations/2019_11_05_043758_create_alumnos_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class CreateAlumnosTable extends Migration
 {
     /**
@@ -16,12 +17,30 @@ class CreateAlumnosTable extends Migration
         Schema::create('alumnos', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('matricula');
-            $table->string('nombre', 40);
-            $table->string('apellido', 40);
-            $table->string('avatar', 50);
+            $table->string('nombre', 50);
+            $table->string('apellido', 50);
             $table->boolean('activo');
             $table->timestamps();
         });
+
+        DB::table('alumnos')->insert([
+            [
+                'matricula' => '157851',
+                'nombre' => 'Guillermo Joaquin',
+                'apellido' => 'Alvarez Ordoñez',
+                'activo' => true,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'matricula' => '147841',
+                'nombre' => 'Adrian',
+                'apellido' => 'GV',
+                'activo' => true,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]
+        ]);
     }
 
     /**
